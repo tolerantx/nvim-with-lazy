86e1dda0cf2b4ada9b15d15d93342278c58cb72b
Abel Sierra <tolerantx@gmail.com>
2024-02-07 10:57:38 -0600

    Add lua

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
+vim.cmd("set colorcolumn=80")
+vim.g.mapleader = " "
