9bc694724a9ba094061f3becce6f2fd4d5f76b8e
Abel Sierra <tolerantx@gmail.com>
2024-02-09 11:15:55 -0600

    Change lualine file path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,18 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula"
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+            path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
-
