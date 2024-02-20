e520e5b91e215584af16914f23e0ac6dd91e13fa
Abel Sierra <tolerantx@gmail.com>
2024-02-20 17:11:27 -0600

    Telescope: add mapper `fw` for grep_string

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,6 +18,7 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
     end
   },
   {
